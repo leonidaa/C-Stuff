e164cbe31fc74e284e1d6b699a628344586cf290
adi g <leonidaa@gmail.com>
2018-10-19 17:00:07 +0300

    rwaew

--- ConsoleApp1Finally/Program.cs
+++ ConsoleApp1Finally/Program.cs
@@ -10,9 +10,31 @@ namespace ConsoleApp1Finally
     {
         static void Main(string[] args)
         {
-            //bbla
-            Console.WriteLine("aeraerg");
-            Console.ReadLine();
+            int i = 123;
+            string s = "Some string";
+            object obj = s;
+
+            try
+            {
+                // invalid conversion  ; obj contains a string , not a numeric type .
+                i = (int)obj;
+                // the following statement is not run . 
+                Console.WriteLine("WriteLine at the end of the try bloc. ");
+            }
+
+            finally
+            {
+                // to run the program in Visual studio , type CTRl+F5. Then 
+                //click Cancel in the error dialog
+                //
+
+                Console.WriteLine("\n Execution of the finally block after an unhandled \n " +
+                    "error depends on how the exception unwind operation is triggered.");
+                Console.WriteLine("i={0}", i);
+                Console.ReadLine();
+
+            }
         }
     }
 }
+
